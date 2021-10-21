45377cdac447f1da9aee427180c9fc0e095f293e
rashidev <rasidaliyarov@gmail.com>
2021-10-21 20:14:18 +0400

    initialized spring boot application

---
+++ src/test/java/com/developia/booksforeverybody/BooksforeverybodyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.developia.booksforeverybody;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksforeverybodyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
