3b7b7d811028460d8e09866908a690759711143c
rashidev <rasidaliyarov@gmail.com>
2021-10-25 19:13:44 +0400

    Minor modifications applied to entity classes

--- src/main/java/com/developia/booksforeverybody/dao/entity/CommentEntity.java
+++ src/main/java/com/developia/booksforeverybody/dao/entity/CommentEntity.java
@@ -4,11 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Data
@@ -30,4 +33,7 @@ public class CommentEntity {
 
     @Column(name = "book_id")
     private Long bookId;
+
+    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.MERGE)
+    private UserEntity user;
 }
