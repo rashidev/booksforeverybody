09570000d8f88d8cc772f5c0dff37203774be69f
rashidev <rasidaliyarov@gmail.com>
2021-11-01 20:39:04 +0400

    add book implemented

--- src/main/java/com/developia/booksforeverybody/service/impl/CartServiceImpl.java
+++ src/main/java/com/developia/booksforeverybody/service/impl/CartServiceImpl.java
@@ -1,21 +1,31 @@
 package com.developia.booksforeverybody.service.impl;
 
+import com.developia.booksforeverybody.dao.entity.BookEntity;
+import com.developia.booksforeverybody.dao.entity.BookStatus;
 import com.developia.booksforeverybody.dao.entity.CartEntity;
 import com.developia.booksforeverybody.dao.entity.UserEntity;
+import com.developia.booksforeverybody.dao.repository.BookRepository;
 import com.developia.booksforeverybody.dao.repository.CartRepository;
 import com.developia.booksforeverybody.dao.repository.UserRepository;
 import com.developia.booksforeverybody.exception.NotFoundException;
 import com.developia.booksforeverybody.service.CartService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class CartServiceImpl implements CartService {
     private final UserRepository userRepository;
     private final CartRepository cartRepository;
+    private final BookRepository bookRepository;
 
-    public CartServiceImpl(UserRepository userRepository, CartRepository cartRepository) {
+
+
+    public CartServiceImpl(UserRepository userRepository,
+                           CartRepository cartRepository, BookRepository bookRepository) {
         this.userRepository = userRepository;
         this.cartRepository = cartRepository;
+        this.bookRepository = bookRepository;
     }
 
     @Override
@@ -34,6 +44,35 @@ public class CartServiceImpl implements CartService {
                 }
         );
        return cart;
+    }
+
+    @Override
+    public void addBookToCart(String username, Long bookId) {
+        UserEntity user = userRepository
+                .findUserByUsername(username).orElseThrow(
+                        () -> {
+                            throw new NotFoundException("User not found!");
+                        }
+                );
+
+        BookEntity book =   bookRepository
+                .findByIdAndStatusIsNot(bookId, BookStatus.DELETED)
+                .orElseThrow(
+                        () -> {
+                            throw  new NotFoundException("Book not found!");
+                        }
+                );
+        CartEntity cart = cartRepository
+                .findByUserId(user.getId()).orElseThrow(
+                        () -> {
+                            throw  new NotFoundException("Cart not found!");
+
+                        }
+                );
+        List<BookEntity> books = cart.getBooks();
+        books.add(book);
+        cart.setBooks(books);
+        cartRepository.save(cart);
     }
 
 }
